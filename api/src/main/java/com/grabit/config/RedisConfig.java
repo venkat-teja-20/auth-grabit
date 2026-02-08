d9a133b4c8e25980f9892063582fd847c4da33a7
Sai Venkata Teja <saivenkatateja714@gmail.com>
2026-02-08 16:56:17 +0530

    Adding Roles and Permissions to Redis implementation is done

--- api/src/main/java/com/grabit/config/RedisConfig.java
+++ api/src/main/java/com/grabit/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.grabit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,19 +15,30 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
-                );
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(config)
-                .build();
-    }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Register JavaTimeModule for Java 8 date/time support
+        objectMapper.registerModule(new JavaTimeModule());
+        // Disable writing dates as timestamps (optional, writes as ISO-8601 strings)
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
+//    @Bean
+//    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory,ObjectMapper objectMapper) {
+//        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+//                .serializeValuesWith(
+//                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
+//                );
+//        return RedisCacheManager.builder(redisConnectionFactory)
+//                .cacheDefaults(config)
+//                .build();
+//    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory,ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -32,10 +46,10 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
 
         // Use JSON serializer for values
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 
         template.afterPropertiesSet();
         return template;
---
+++ api/src/main/java/com/grabit/helper/auth/RedisHelper.java
@@ -0,0 +1,39 @@
+package com.grabit.helper.auth;
+
+import com.grabit.entity.Role;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
+
+@Log4j2
+public class RedisHelper {
+
+    private RedisTemplate<String,Object> redisTemplate;
+
+    public RedisHelper(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void addOrUpdateRoleDetailsToRedis(Role role,String cacheName){
+        try{
+            String key=cacheName+"_"+role.getId();
+            redisTemplate.opsForValue().set(key,role);
+            log.info("Role added/updated to Redis : "+role.getId());
+            printRedisContents(cacheName);
+        } catch (Exception e){
+            log.error("Error while adding/updating role in redis : "+e);
+        }
+    }
+
+    public void printRedisContents(String cacheName){
+        try{
+            Set<String> keys=redisTemplate.keys(cacheName+"*");
+            for(String key:keys){
+                System.out.println(redisTemplate.opsForValue().get(key));
+            }
+        } catch (Exception e){
+            log.error("Error while adding/updating role in redis : "+e);
+        }
+    }
+}
